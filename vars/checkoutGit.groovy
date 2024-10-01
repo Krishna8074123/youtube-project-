6de6ffbcf7c9e2f61605db01a34602f6937d450f
krishna <gangulak36@gmail.com>
2024-10-01 16:03:49 +0530

    bgn

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -1,3 +1,3 @@
 def call(String gitUrl, String gitBranch) {
    checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/Krishna8074123/jenkins-yt.git']])
-
+}
